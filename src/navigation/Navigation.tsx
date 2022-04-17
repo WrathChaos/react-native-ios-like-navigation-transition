ec3d50e8fb2a066598576c71ba9dadc86f3149d5
FreakyCoder <kurayogun@gmail.com>
2022-04-18 00:42:32 +0300

    fix: android navigation

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,18 +1,47 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { TransitionPresets } from "@react-navigation/stack";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
 import { isAndroid } from "@freakycoder/react-native-helpers";
 import { NavigationContainer } from "@react-navigation/native";
 import { isReadyRef, navigationRef } from "react-navigation-helpers";
 import HomeScreen from "../screens/Home";
 import DetailScreen from "../screens/Detail";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const Navigation = () => {
   React.useEffect((): any => {
     return () => (isReadyRef.current = false);
   }, []);
+
+  const RootStack = () => (
+    <Stack.Navigator
+      initialRouteName="home"
+      screenOptions={{
+        gestureEnabled: true,
+        ...(isAndroid && TransitionPresets.ModalTransition),
+      }}
+    >
+      <Stack.Group
+        screenOptions={{
+          headerShown: false,
+          presentation: "modal",
+        }}
+      >
+        <Stack.Screen name="home" component={HomeScreen} />
+      </Stack.Group>
+      <Stack.Group
+        screenOptions={{
+          headerShown: false,
+          presentation: "modal",
+        }}
+      >
+        <Stack.Screen name="detail" component={DetailScreen} />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
 
   return (
     <NavigationContainer
@@ -22,28 +51,11 @@ const Navigation = () => {
       }}
     >
       <Stack.Navigator
-        initialRouteName="home"
-        screenOptions={{
-          gestureEnabled: true,
-          ...(isAndroid && TransitionPresets.ModalPresentationIOS),
-        }}
+        screenOptions={() => ({
+          headerShown: false,
+        })}
       >
-        <Stack.Group
-          screenOptions={{
-            headerShown: false,
-            presentation: "modal",
-          }}
-        >
-          <Stack.Screen name="home" component={HomeScreen} />
-        </Stack.Group>
-        <Stack.Group
-          screenOptions={{
-            headerShown: false,
-            presentation: "modal",
-          }}
-        >
-          <Stack.Screen name="detail" component={DetailScreen} />
-        </Stack.Group>
+        <Stack.Screen name="home-stack" children={RootStack} />
       </Stack.Navigator>
     </NavigationContainer>
   );
